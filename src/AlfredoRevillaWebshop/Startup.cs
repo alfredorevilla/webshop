c5b394a9ee078aa9b98ab8b3370d15666bbff732
Alfredo Revilla <alfredorevilla@gmail.com>
2017-07-27 12:52:16 -0500

    New default route. Registered service dependency.

--- src/AlfredoRevillaWebshop/Startup.cs
+++ src/AlfredoRevillaWebshop/Startup.cs
@@ -1,13 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Linq;
+using System;
+using AlfredoRevillaWebshop.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
 
 namespace AlfredoRevillaWebshop
 {
@@ -25,14 +23,6 @@ namespace AlfredoRevillaWebshop
 
         public IConfigurationRoot Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // Add framework services.
-            services.AddMvc();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
         {
             loggerFactory.AddConsole(Configuration.GetSection("Logging"));
@@ -54,8 +44,15 @@ namespace AlfredoRevillaWebshop
             {
                 routes.MapRoute(
                     name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
+                    template: "{controller=Products}/{action=Index}");
             });
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+
+            services.AddTransient<ProductService>();
         }
     }
 }
