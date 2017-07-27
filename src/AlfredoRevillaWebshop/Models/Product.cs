01d313d6993f240cd58d3e0f81fb0a5d94b556d0
Alfredo Revilla <alfredorevilla@gmail.com>
2017-07-26 19:54:18 -0500

    Basic webapp structure.

---
+++ src/AlfredoRevillaWebshop/Models/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlfredoRevillaWebshop.Models
+{
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MaxLength(255)]
+        public string Title { get; set; }
+
+        [Required]
+        [MaxLength(13)]
+        public string MPN { get; set; }
+    }
+}
